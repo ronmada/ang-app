6da330febd44d302ee6d9021ce005f91fed1c69e
sagi arieli <sagiarieli@gmail.com>
2019-05-18 20:08:27 +0300

    homo

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder } from '@angular/forms';
 import { FormArray } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-name-editor',
@@ -14,10 +15,12 @@ export class NameEditorComponent {
   readonly ROOT_URL_local = 'http://127.0.0.1:5000'
   courses : any
   courseid : any
-  smgroup: String[] = []
+  somecourse : any
+  couresy: Observable<String>
+  smgroup: any[] = []
   nameForm = this.fb.group({
-    add_single_course: [''],
-    sm_group: ['']
+    add_single_course: ['11004'],
+    sm_group: ['11005']
   })
 
   constructor(private fb: FormBuilder, private http: HttpClient) { }
@@ -35,11 +38,17 @@ export class NameEditorComponent {
   */
   getOneCourse(){
     let params= new HttpParams().set('courseid' , this.courseid)
-    console.log(this.courseid)
     this.courses = this.http.get(this.ROOT_URL_local + '/getcorjs', {params})
   }
   push_one_to_sm(){
-    this.smgroup.push(this.nameForm.value.sm_group)
-    console.log(this.smgroup)
+    let acourse = this.nameForm.value.sm_group
+    //console.log(this.smgroup)
+    this.getOne(acourse)
+  }
+  getOne(courseid){
+    let params= new HttpParams().set('courseid' , courseid)
+    this.somecourse = this.http.get(this.ROOT_URL_local + '/getcorjs', {params}).subscribe()
+    this.smgroup.push(this.somecourse[0])
+
   }
 }
