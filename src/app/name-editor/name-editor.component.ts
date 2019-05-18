02d8f6b92966101b1198a7df92bf323a9712d016
Ron Wexler <ronmada@s.braude.ac.il>
2019-05-18 18:40:40 +0300

    hey

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -14,25 +14,32 @@ export class NameEditorComponent {
   readonly ROOT_URL_local = 'http://127.0.0.1:5000'
   courses : any
   courseid : any
+  smgroup: String[] = []
   nameForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['']
+    add_single_course: [''],
+    sm_group: ['']
   })
 
   constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
-    this.courseid = this.nameForm.value.firstName
+    this.courseid = this.nameForm.value.add_single_course
     this.getOneCourse()
-    console.warn(this.nameForm.value.firstName);
+    console.warn(this.nameForm.value.add_single_course);
   }
+  /*
   get aliases() {
     return this.nameForm.get('firstName') as FormArray;
   }
+  */
   getOneCourse(){
     let params= new HttpParams().set('courseid' , this.courseid)
     console.log(this.courseid)
     this.courses = this.http.get(this.ROOT_URL_local + '/getcorjs', {params})
+  }
+  push_one_to_sm(){
+    this.smgroup.push(this.nameForm.value.sm_group)
+    console.log(this.smgroup)
   }
 }
