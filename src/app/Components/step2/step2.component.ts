d0d9fce5a61164277c1767d0f5248d9e5dac6550
Ron Wexler <ronmada@s.braude.ac.il>
2019-06-07 16:17:41 +0300

    update

--- src/app/Components/step2/step2.component.ts
+++ src/app/Components/step2/step2.component.ts
@@ -1,37 +1,59 @@
-import { Component , OnInit, Input } from '@angular/core';
-import { CourseService } from '../../Services/course.service';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import {FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms';
+import { Component, OnInit, Input } from "@angular/core";
+import { CourseService } from "../../Services/course.service";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Struct } from "../../models/Struct";
 
 @Component({
-  selector: 'app-step2',
-  templateUrl: './step2.component.html',
-  styleUrls: ['./step2.component.css']
-
+  selector: "app-step2",
+  templateUrl: "./step2.component.html",
+  styleUrls: ["./step2.component.css"]
 })
 export class Step2Component implements OnInit {
   button_value: string;
-  displayParameters: string[] = ['ID','Name'];
-  chosen : string
-
-  public radiogroup: FormGroup;
-  option3Disable=true;
-  testForm = this.fb.group({
-           option1: [{disabled:true}],
-           option2: [],
-           option3: [],
-
-   });
+  displayParameters: string[] = ["ID", "Name"];
+  chosen_course: any[] = [];
+  model: string[];
+  struct: Struct;
+  foods: any[] = [
+    { value: "steak-0", viewValue: "Steak" },
+    { value: "pizza-1", viewValue: "Pizza" },
+    { value: "tacos-2", viewValue: "Tacos" }
+  ];
   constructor(
-    public courseService : CourseService,
+    public courseService: CourseService,
     private route: ActivatedRoute,
     private router: Router,
-    private fb: FormBuilder,
-    ) { }
-    ngOnInit(){
-
+    private fb: FormBuilder
+  ) {}
+  ngOnInit() {
+    this.struct = this.courseService.getstruct();
+    console.log("Struct is:", this.struct);
+  }
+  onSelect(course) {
+    for (let group of course.__Course__.groups){
+      this.chosen_course.push(group)
     }
-    handleChange(evt){
-    console.log("Event:" , evt);
-}
+    console.log("chosen course groups:", this.chosen_course);
+    this.check_course_types(this.chosen_course);
+  }
+
+  check_course_types(course) {
+    let q_a: boolean = false;
+    let lect: boolean = false;
+    let pract: boolean = false;
+    for (let cor of course) {
+      console.log("cor", cor);
+      console.log("COURSE length" , cor.__Course_Group__.lectures.length)
+      // for (let co of cor) {
+      //   console.log("LEngTH1", co.__Course_Group__.lectures.length);
+      //   console.log("lengthL" , co.length)
+      //   if (co.length){
+      //      lect = true;
+      //      console.log("there are X lectures:", co.length);
+      //     //  this.lectures =
+      //   }
+      // }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule  , CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CourseService } from "./Services/course.service";
-import { MatTableModule ,  } from "@angular/material";
-import { ComboBoxComponent } from './Components/combo-box/combo-box.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatRadioModule } from '@angular/material';
-import { LoadScreenComponent } from './Components/load-screen/load-screen.component';
-
+import { MatTableModule } from "@angular/material";
+import { ComboBoxComponent } from "./Components/combo-box/combo-box.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatRadioModule } from "@angular/material";
+import { LoadScreenComponent } from "./Components/load-screen/load-screen.component";
+import { MatSelectModule } from "@angular/material/select";
 @NgModule({
-  declarations: [AppComponent, routingComponents, ComboBoxComponent, LoadScreenComponent],
+  declarations: [
+    AppComponent,
+    routingComponents,
+    ComboBoxComponent,
+    LoadScreenComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,11 +29,11 @@ import { LoadScreenComponent } from './Components/load-screen/load-screen.compon
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatRadioModule
+    MatRadioModule,
+    MatSelectModule
   ],
   providers: [CourseService],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
-
 })
 export class AppModule {}
