adac0c2cfc61b0f42c6f049ea559cdabb0840ad0
Ron Wexler <ronmada@s.braude.ac.il>
2019-06-10 02:27:29 +0300

    k

--- src/app/Components/step2/step2.component.ts
+++ src/app/Components/step2/step2.component.ts
@@ -31,9 +31,12 @@ export class Step2Component implements OnInit {
     console.log("Struct is:", this.struct);
   }
   onSelect(course) {
-    for (let group of course.__Course__.groups){
-      this.chosen_course.push(group)
+    let course_group: any[] = [];
+    let emptyAray = [];
+    for (let group of course.__Course__.groups) {
+      course_group.push(group);
     }
+    this.chosen_course = emptyAray.concat(course_group);
     console.log("chosen course groups:", this.chosen_course);
     this.check_course_types(this.chosen_course);
   }
@@ -44,7 +47,10 @@ export class Step2Component implements OnInit {
     let pract: boolean = false;
     for (let cor of course) {
       console.log("cor", cor);
-      console.log("COURSE length" , cor.__Course_Group__.lectures.length)
+      console.log("COURSE length", cor.__Course_Group__.lectures.length);
+      if (cor.__Course_Group__.lectures.length > 0) lect = true;
+      if (cor.__Course_Group__.practices.length > 0) pract = true;
+      if (cor.__Course_Group__.q_and_as.length > 0) q_a = true;
       // for (let co of cor) {
       //   console.log("LEngTH1", co.__Course_Group__.lectures.length);
       //   console.log("lengthL" , co.length)
@@ -55,5 +61,7 @@ export class Step2Component implements OnInit {
       //   }
       // }
     }
+    console.log("lects" , lect , "practs" , pract , "q_a" , q_a)
+
   }
 }
